7b9c94e81abe7bbc7b94a4e63f7c0ee6e2aeb51f
ramneekriar <ramneek.riar@torontomu.ca>
2025-01-23 23:03:52 -0500

    Added Put Mapping for updateStudent

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,9 +1,11 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -34,5 +36,22 @@ public class StudentService {
             throw new IllegalStateException("Student with id " + studentId + " does not exist.");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student with " + studentId + " does not exist."));
+
+        if (name != null && !name.isEmpty() && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+        if (email != null && !email.isEmpty() && !Objects.equals(student.getEmail(), email)){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()){
+                throw new IllegalStateException("This email is already taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
